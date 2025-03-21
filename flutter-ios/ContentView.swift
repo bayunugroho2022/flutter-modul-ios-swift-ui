938b9eb149ee83ed6972740c4f0b8ef13d343af0
Bayu Nugroho <bayu404.dart@gmail.com>
2025-03-21 16:09:33 +0700

    Initial Commit

---
+++ flutter-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flutter-ios
+//
+//  Created by Bayu Nugroho on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flutter-ios/flutter_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_iosApp.swift
+//  flutter-ios
+//
+//  Created by Bayu Nugroho on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct flutter_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
